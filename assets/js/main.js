39246764ca02f8772f7350d55c01e2cb03a95408
eriveltondasilva <eriveltondasilva13@gmail.com>
2023-02-18 02:13:23 -0300

    js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,8 @@
 // # CALCULADORA
+// TODO: refatorar o código focando em enxugar as partes repetidas
 
+
+// -Constantes
 const OPERATION = document.querySelector(".js-screen__operation");
 const RESULT = document.querySelector(".js-screen__result");
 const CLEAR = document.querySelector(".js-keyboard__clear");
@@ -15,103 +18,209 @@ const OPERATORS = document.querySelectorAll(".js-keyboard__operator");
 
 
 
+// -Eventos
 // Limpar a tela ao pressionar no botão "C".
 CLEAR.addEventListener("click", _clear);
 
+
+// Deletar um por um os dígitos da tela.
+DELETE.addEventListener("click", _delete);
+
+
+// Imprimir ponto na tela
+DOT.addEventListener("click", typeDot);
+
+
+// Imprimir zeros na tela
+ZERO.addEventListener("click", typeZero);
+
+
+// Imprimir os números na tela
+for (let i = 0; i < NUMBERS.length; i++) {
+    NUMBERS[i].addEventListener("click", typeNumber);
+}
+
+
+// Imprimir os operadores matemáticos na tela
+for (let i = 0; i < OPERATORS.length; i++) {
+    OPERATORS[i].addEventListener("click", typeOperator);
+}
+
+
+
+
+
+// -Funções
+// 
 function _clear() {
     OPERATION.innerHTML = "";
     RESULT.innerHTML = "";
 };
 
 
-// Deletar um por um os dígitos da tela.
-DELETE.addEventListener("click", _delete);
-
+//
 function _delete() {
-    RESULT.innerHTML = RESULT.innerText.slice(0, -1);
-    OPERATION.innerHTML = RESULT.innerHTML;
+    let lastOperator = OPERATION.innerText.slice(-1);
+
+    if (
+        lastOperator != "+" &&
+        lastOperator != "-" &&
+        lastOperator != "×" &&
+        lastOperator != "÷"
+    ) {
+        RESULT.innerHTML = RESULT.innerText.slice(0, -1);
+        OPERATION.innerHTML = OPERATION.innerText.slice(0, -1);
+    };
 };
 
 
-// Imprimir zeros na tela
-DOT.addEventListener("click", typeDot);
-
+//
 function typeDot() {
     let display = RESULT.innerHTML.length;
     let dot = RESULT.innerHTML.includes(",");
+    let operation = OPERATION.innerHTML;
+    let operatorPlus = operation.indexOf("+");
+    let operatorMinus = operation.indexOf("-");
+    let operatorTimes = operation.indexOf("×");
+    let operatorDivide = operation.indexOf("÷");
 
-    if (display < 8 && display > 0) {
+    if (display < 9 && display > 0) {
         if (dot < 1) {
             RESULT.innerHTML += this.value;
         }
     }
 
-    OPERATION.innerHTML = RESULT.innerHTML;
+    if (
+        operatorPlus >= 0 ||
+        operatorMinus >= 0 ||
+        operatorTimes >= 0 ||
+        operatorDivide >= 0
+    ) {
+        if (display < 9) {
+            OPERATION.innerHTML += RESULT.innerHTML.slice(-1);
+        }
+    } else {
+        OPERATION.innerHTML = RESULT.innerHTML;
+    }
 }
 
 
-// Imprimir zeros na tela
-ZERO.addEventListener("click", typeZero);
-
+//
 function typeZero() {
     let display = RESULT.innerHTML.length;
     let zero = RESULT.innerHTML.charAt(0);
+    let operation = OPERATION.innerHTML;
+    let operatorPlus = operation.indexOf("+");
+    let operatorMinus = operation.indexOf("-");
+    let operatorTimes = operation.indexOf("×");
+    let operatorDivide = operation.indexOf("÷");
 
-    if (display < 8) {
+
+    if (display < 9) {
         if (zero == "" || zero > 0 || display > 1) {
             RESULT.innerHTML += this.value;
         }
     };
 
-    OPERATION.innerHTML = RESULT.innerHTML;
+    if (
+        operatorPlus >= 0 ||
+        operatorMinus >= 0 ||
+        operatorTimes >= 0 ||
+        operatorDivide >= 0
+    ) {
+        if (display < 9) {
+            OPERATION.innerHTML += RESULT.innerHTML.slice(-1);
+        }
+    } else {
+        OPERATION.innerHTML = RESULT.innerHTML;
+    }
 }
 
 
+//
+function typeNumber() {
+    let display = RESULT.innerHTML.length;
+    let zero = RESULT.innerHTML.charAt(0);
+    let operation = OPERATION.innerHTML;
+    let operatorPlus = operation.indexOf("+");
+    let operatorMinus = operation.indexOf("-");
+    let operatorTimes = operation.indexOf("×");
+    let operatorDivide = operation.indexOf("÷");
 
-// Imprimir os números na tela
-for (let i = 0; i < NUMBERS.length; i++) {
-    NUMBERS[i].addEventListener("click", typeNumber);
-
-    function typeNumber() {
-        let display = RESULT.innerHTML.length;
-        let zero = RESULT.innerHTML.charAt(0);
-
-        if (display < 8) {
-            if (zero == "0" && display < 1) {
-                RESULT.innerHTML = "";
-                RESULT.innerHTML += this.value;
-            } else {
-                RESULT.innerHTML += this.value;
-            }
+    if (display < 9) {
+        if (zero == "0" && display == 1) {
+            RESULT.innerHTML = "";
+            RESULT.innerHTML += this.value;
+        } else {
+            RESULT.innerHTML += this.value;
         };
+    };
 
+
+    if (
+        operatorPlus >= 0 ||
+        operatorMinus >= 0 ||
+        operatorTimes >= 0 ||
+        operatorDivide >= 0
+    ) {
+        if (display < 9) {
+            OPERATION.innerHTML += RESULT.innerHTML.slice(-1);
+        }
+    } else {
         OPERATION.innerHTML = RESULT.innerHTML;
+    }
+
+};
+
+
+function typeOperator() {
+    let display = OPERATION.innerHTML.length;
+    let lastOperator = OPERATION.innerText.charAt(display - 1);
+    let operation = OPERATION.innerHTML;
+    let operatorPlus = operation.indexOf("+");
+    let operatorMinus = operation.indexOf("-");
+    let operatorTimes = operation.indexOf("×");
+    let operatorDivide = operation.indexOf("÷");
+
+    if (display > 0) {
+        if (
+            operatorPlus < 0 &&
+            operatorMinus < 0 &&
+            operatorTimes < 0 &&
+            operatorDivide < 0
+        ) {
+            if (
+                lastOperator != "+" &&
+                lastOperator != "-" &&
+                lastOperator != "×" &&
+                lastOperator != "÷"
+            ) {
+                OPERATION.innerHTML += this.value;
+                RESULT.innerHTML = "";
+            }
+        } else if (
+            lastOperator == "+" ||
+            lastOperator == "-" ||
+            lastOperator == "×" ||
+            lastOperator == "÷"
+        ) {
+            OPERATION.innerHTML = OPERATION.innerText.slice(0, -1);
+            OPERATION.innerHTML += this.value;
+        };
     };
 };
 
 
-// Imprimir os operadores matemáticos na tela
-for (let i = 0; i < OPERATORS.length; i++) {
-    OPERATORS[i].addEventListener("click", typeOperator);
 
-    function typeOperator() {
-        let display = OPERATION.innerHTML.length;
-        let operator = OPERATION.innerHTML.includes(this.value);
+// TODO:
+EQUAL.addEventListener("click", typeEqual);
 
-        if (display > 0 && display < 8) {
-            if (operator < 1) {
-
-                OPERATION.innerHTML += this.value;
-                RESULT.innerHTML = "";
-
-
-            }
-        };
-    };
+function typeEqual() {
 
 }
 
 
+// lastOperator != "+" || lastOperator != "-" || lastOperator != "&times" || lastOperator != "&divide"
 
 
 // EQUAL.addEventListener("click", _equal);
